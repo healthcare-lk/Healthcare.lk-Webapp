a052da2848da41e6546d98cbcc7567a968a1c0f5
Gayathree <gayarathnayaka96@gmail.com>
2021-08-30 16:19:34 +0530

    POST(Login) part

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -19,5 +19,22 @@ const userCtrl = {
             return res.status(500).json({msg: err.message})
         }
     }
+},
+login: async(req, res) =>{
+    try {
+        const {email, password} = req.body;
+
+        const user = await Users.findOne({email})
+        if(!user) return res.status(400).json({msg: "User does not exist."})
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        if(!isMatch) return res.status(400).json({msg: "Incorrect password."})
+
+        res.json({"Login Success!"})
+
+    } catch (err) {
+        return res.status(500).json({msg: err.message})
+
+    }
 }
 module.exports = userCtrl
