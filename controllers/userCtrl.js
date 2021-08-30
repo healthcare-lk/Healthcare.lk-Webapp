74209f53af12e6475fa123ba49c6b6ac4e89793c
SandunikaMaduhari <89002199+SandunikaMaduhari@users.noreply.github.com>
2021-08-30 17:19:13 +0530

    Update userCtrl.js

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -56,23 +56,43 @@ const userCtrl = {
 
         }
         
-    }
-},
-login: async(req, res) =>{
-    try {
-        const {email, password} = req.body;
+    },
 
-        const user = await Users.findOne({email})
-        if(!user) return res.status(400).json({msg: "User does not exist."})
+    login: async(req, res) =>{
+        try {
+            const {email, password} = req.body;
+    
+            const user = await Users.findOne({email})
+            if(!user) return res.status(400).json({msg: "User does not exist."})
+    
+            const isMatch = await bcrypt.compare(password, user.password)
+            if(!isMatch) return res.status(400).json({msg: "Incorrect password."})
+    
+            // If login success , create access token and refresh token
+            const accesstoken = createAccessToken({id: user._id})
+            const refreshtoken = createRefreshToken({id: user._id})
 
-        const isMatch = await bcrypt.compare(password, user.password)
-        if(!isMatch) return res.status(400).json({msg: "Incorrect password."})
+            res.cookie('refreshtoken', refreshtoken, {
+                httpOnly: true,
+                path: '/user/refresh_token',
+                maxAge: 7*24*60*60*1000 //7D
+            })
 
-        res.json({"Login Success!"})
+            res.json({accesstoken})
+    
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+    
+        }
+    },
 
-    } catch (err) {
-        return res.status(500).json({msg: err.message})
-
+    logout: async (req, res) =>{
+        try {
+            res.clearCookie('refreshtoken', {path: '/user/refresh_token'})
+            return res.json({msg: "Logged out"})
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
     }
 }
 
