d55be3ed6bcd4984c0a288e8c38861d46a4dd271
kasunliyanagamage <89001337+kasunliyanagamage@users.noreply.github.com>
2021-09-01 08:48:07 +0530

    Update Products.js

--- healthapp/src/components/mainPages/products/Products.js
+++ healthapp/src/components/mainPages/products/Products.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {GlobalState} from '../../../GlobalState'
+import ProductItem from '../utils/productItem/ProductItem'
+import Filters from './Filters'
+
 
 function Products() {
-    return(
-        <div>
-            Products List
+    const state = useContext(GlobalState)
+    const [products] = state.productsAPI.products
+    const [isAdmin] = state.userAPI.isAdmin
+    const [token] = state.token
+    const [callback, setCallback] = state.productsAPI.callback
+
+    return (
+        <>
+        <Filters/>
+        <div className="products">
+           {
+               products.map(product => {
+                   return <ProductItem key={product._id} product={product}
+                   isAdmin={isAdmin} token={token} callback={callback} setCallback={setCallback}/>
+               })
+           }
         </div>
+        </>
+
     )
-    
 }
+
 export default Products
