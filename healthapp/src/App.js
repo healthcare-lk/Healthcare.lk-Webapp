e75e8851d5688af3389c868e42a6563cae0703e7
SandunikaMaduhari <89002199+SandunikaMaduhari@users.noreply.github.com>
2021-09-03 07:36:36 +0530

    Update App.js

--- healthapp/src/App.js
+++ healthapp/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {BrowserRouter as Router} from 'react-router-dom'
 import {DataProvider} from './GlobalState'
-import Header from "./components/headers/Header"
+import Header from "./components/headers/Header";
 import MainPages from './components/mainpages/Pages'
 
 function App() {
